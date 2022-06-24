5b2bbff3c72893583c9bc1605c74be06a34d915c
Mateo <mateodibenedetto01@gmail.com>
2022-06-23 23:21:19 -0300

    clase03

---
+++ clase3/03.js
@@ -0,0 +1,30 @@
+/// *** BUCLE FOR *** ///
+// for (let i = 0; i < 5; i++) {
+//     let nombre_usuario = prompt("Ingrese su nombre");
+//     console.log("Bienvenido/a:", nombre_usuario);
+//     console.log("El contadore está en:", i);
+// }
+
+// Ejercicios
+// Pedir un número del 1 al 10 por teclado y mostrar la tabla del mismo
+let num = prompt("Ingrese un numero para mostrar su tabla de multiplicar");
+
+for (let i = 0; i < 10; i++) {
+    console.log(`${num} x ${i} = ${parseInt(num) * i}`);
+}
+
+// Se van a ingresar 10 numeros, informo si el numero es negativo o positivo
+for (let i = 0; i < 10; i++) {
+    let num = prompt("Ingrese un numero: ");
+
+    let numP = parseInt(num);
+    
+    if (numP < 0) {
+        console.log(`${numP} es negativo`);
+    } else if (numP >= 0)  {
+        console.log(`${numP} es positivo`);
+    } else {
+        console.log(`${numP} no es un numero`);
+    }
+    
+}
